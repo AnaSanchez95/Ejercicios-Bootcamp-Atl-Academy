913796942eb1223c6139648292dc513bc4b928a1
Ana <120680319+AnaSanchez95@users.noreply.github.com>
2023-06-06 21:24:40 +0200

    Hecho ejercicio 1

---
+++ Clase-5/src/CalculadoraEdadPerros.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CalculadoraEdadPerros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa la edad de su perro: ");
+        Integer dogAge = scanner.nextInt();
+
+        Integer humanAge = dogAge * 7;
+        System.out.println("En años humanos tu perro tiene " + humanAge + " años");
+
+    }
+
+}
